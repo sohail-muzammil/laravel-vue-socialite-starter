65b78d34cdadc9c1e1ee13d96dfcd207f4f21820
sohail-muzammil <sohail.muzammil20@gmail.com>
2025-03-23 02:13:29 +0800

    Minor Improvements

--- resources/js/components/SocialiteLinks.vue
+++ resources/js/components/SocialiteLinks.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { computed } from "vue";
-import { usePage } from "@inertiajs/vue3";
+import { usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
 const page = usePage();
 const providers = computed(() => page.props.oauth_providers);
@@ -8,15 +8,18 @@ const providers = computed(() => page.props.oauth_providers);
 
 <template>
     <div
-        class="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-        <span class="relative z-10 bg-background px-2 text-muted-foreground">
-            Or continue with
-        </span>
+        class="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border"
+    >
+        <span class="relative z-10 bg-background px-2 text-muted-foreground"> Or continue with </span>
     </div>
 
     <div class="grid grid-cols-2 gap-4">
-        <a v-for="provider in providers" :key="provider" :href="route('auth.socialite.redirect', provider.name)"
-            class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 capitalize">
+        <a
+            v-for="provider in providers"
+            :key="provider"
+            :href="route('auth.socialite.redirect', provider.name)"
+            class="inline-flex h-9 items-center justify-center gap-2 whitespace-nowrap rounded-md border border-input bg-background px-4 py-2 text-sm font-medium capitalize shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+        >
             <span v-html="provider.icon"></span>
             <span>{{ provider.name }}</span>
         </a>
--- resources/js/pages/settings/Socialite.vue
+++ resources/js/pages/settings/Socialite.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-import { computed } from "vue";
-import { Head, usePage, Link } from '@inertiajs/vue3';
+import { Head, Link, usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
 
 import HeadingSmall from '@/components/HeadingSmall.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import SettingsLayout from '@/layouts/settings/Layout.vue';
-import { type BreadcrumbItem, type SharedData, type User } from '@/types';
+import { type BreadcrumbItem } from '@/types';
+
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 
 interface Props {
     socialAccounts?: number[];
@@ -26,51 +28,52 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 <template>
     <AppLayout :breadcrumbs="breadcrumbs">
-
         <Head title="Social Accounts settings" />
 
         <SettingsLayout>
             <div class="space-y-6">
-                <HeadingSmall title="Social Accounts information"
-                    description="Connect or disconnect your social accounts" />
+                <HeadingSmall title="Social Accounts information" description="Connect or disconnect your social accounts" />
 
                 <div class="overflow-x-auto">
-                    <table class="min-w-full bg-white dark:bg-gray-800">
-                        <thead class="bg-gray-50 dark:bg-gray-700">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400">
-                                    Provider
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium uppercase text-gray-500 dark:text-gray-400">
-                                    Action
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody class="divide-y divide-gray-200 dark:divide-gray-600 capitalize">
-                            <tr v-for="(provider, index) in providers" :key="index" class="hover:bg-gray-50 dark:hover:bg-gray-700">
-                                <td class="px-6 py-4 whitespace-nowrap flex items-center">
+                    <Table>
+                        <TableCaption>A list of your recent invoices.</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead class="w-[100px]"> Provider </TableHead>
+                                <TableHead class="text-right"> Action </TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            <TableRow v-for="(provider, index) in providers" :key="index">
+                                <TableCell class="flex items-center gap-2 font-medium capitalize">
                                     <span v-html="provider.icon" class="mr-2"></span>
-                                    {{ provider.name }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <Link v-if="socialAccounts && socialAccounts.includes(provider.id)" :href="route('auth.socialite.disconnect', { driver: provider.id })"
-                                        method="DELETE" as="button"
+                                    <span>{{ provider.name }}</span>
+                                </TableCell>
+                                <TableCell class="text-right">
+                                    <Link
+                                        v-if="socialAccounts && socialAccounts.includes(provider.id)"
+                                        :href="route('auth.socialite.disconnect', { driver: provider.id })"
+                                        method="DELETE"
+                                        as="button"
                                         :tabindex="6"
-                                        class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90 h-9 px-4 py-2">
+                                        class="inline-flex h-9 items-center justify-center gap-2 whitespace-nowrap rounded-md bg-destructive px-4 py-2 text-sm font-medium text-destructive-foreground shadow-sm ring-offset-background transition-colors hover:bg-destructive/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+                                    >
                                         Disconnect
                                     </Link>
-                                    <a v-else :href="route('auth.socialite.redirect', provider.name)"
+                                    <a
+                                        v-else
+                                        :href="route('auth.socialite.redirect', provider.name)"
                                         :tabindex="6"
-                                        class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2">
+                                        class="inline-flex h-9 items-center justify-center gap-2 whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+                                    >
                                         Connect
                                     </a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
                 </div>
             </div>
         </SettingsLayout>
     </AppLayout>
 </template>
-
